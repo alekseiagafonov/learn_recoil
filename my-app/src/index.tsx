bd4227157336064376e9be8cd2c0e2a542dc5bce
Aleksei Agafonov <wecannotsee@gmail.com>
2021-04-03 21:59:57 +0300

    fix import

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "../src/components/App;
+import App from "../src/components/App";
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
