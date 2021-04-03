37206e82f23e2df117bdd8db786350a39df1a668
Aleksei Agafonov <wecannotsee@gmail.com>
2021-04-03 21:59:04 +0300

    todo list filter + todo list stats

---
+++ my-app/src/components/TodoListFilters.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { todoListFilterState } from "./atoms";
+
+export default function TodoListFilters() {
+  const [filter, setFilter] = useRecoilState(todoListFilterState);
+
+  return (
+    <div>
+      Filter:
+      <select
+        value={filter}
+        onChange={(event) => setFilter(event.target.value)}
+      >
+        <option value="Show All"> All </option>
+        <option value="Show completed ">Completed</option>
+        <option value="Show Uncompleted">Uncompleted</option>
+      </select>
+    </div>
+  );
+}
---
+++ my-app/src/components/TodoListStats.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { todoListStatsState } from "../selectors";
+
+export default function TodoListStats() {
+  const {
+    totalNum,
+    totalCompletedNum,
+    totalUncompletedNum,
+    percentCompleted,
+  } = useRecoilValue(todoListStatsState);
+
+  const formattedPercentComplete = Math.round(percentCompleted * 100);
+
+  return (
+    <ul>
+      <li>Total items: {totalNum}</li>
+      <li>Items completed: {totalCompletedNum}</li>
+      <li>Items not completed: {totalUncompletedNum}</li>
+      <li>Percent completed: {formattedPercentComplete}</li>
+    </ul>
+  );
+}
